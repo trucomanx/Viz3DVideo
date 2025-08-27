a96fee15826396601d193b88c8fc441595626f2d
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2025-08-26 22:02:10 -0300

    ordenando

--- src/viz3dvideo/surf/__init__.py
+++ src/viz3dvideo/surf/__init__.py
@@ -7,7 +7,3 @@ Provides functions to animate surfaces by following a camera path
 or by rotating around the object.
 """
 
-from .path import animate_path
-from .rotate import animate_rotate
-
-__all__ = ["animate_path", "animate_rotate"]
